55a1843a1afbd6c2da15aa94f8fa4c21b4950d31
MaiaRojas <maia.rt.46@gmail.com>
2018-07-10 20:57:51 -0500

    email and password

---
+++ app.js
@@ -0,0 +1,49 @@
+const login = document.getElementById('login');
+const logout = document.getElementById('logout');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const btnRegister = document.getElementById('btnRegister')
+const btnLogin = document.getElementById('btnLogin')
+const btnLogout = document.getElementById('btnLogout')
+
+btnRegister.addEventListener('click', () => {
+  firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+  .then(() => {
+    console.log('Usuario Creado');
+    login.classList.add("hiden");
+    logout.classList.remove("hiden");
+    username.innerHTML = ' Hola Claudia';
+
+  })
+  .catch(function(error) {
+    console.log(error.code ,' : ' , error.message);
+  });
+})
+
+
+btnLogin.addEventListener('click', () => {
+  firebase.auth().signInWithEmailAndPassword(email.value, password.value)
+  .then (() => {
+    console.log('Verificado')
+    login.classList.add("hiden");
+    logout.classList.remove("hiden");
+
+    var user = firebase.auth().currentUser;
+
+    username.innerHTML = user.email;
+  })
+  .catch(function(error) {
+    console.log('Contraseña Incorrecta')
+  });
+})
+
+btnLogout.addEventListener('click', () => {
+  firebase.auth().signOut().then(function() {
+    console.log('Cerro Sesión');
+    login.classList.remove("hiden");
+    logout.classList.add("hiden");
+  }).catch(function(error) {
+    console.log('Error al cerrar Sesión');
+  });
+})
