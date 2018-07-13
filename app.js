8aed39dbe0f1c811c69c3c0509fe29e89e71335c
MaiaRojas <maia.rt.46@gmail.com>
2018-07-13 01:58:46 -0500

    avance

--- app.js
+++ app.js
@@ -8,22 +8,78 @@ const btnLogin = document.getElementById('btnLogin')
 const btnLogout = document.getElementById('btnLogout')
 const btnFacebook = document.getElementById('btnFacebook');
 const btnGoogle = document.getElementById('btnGoogle');
-
+const bd = document.getElementById('bd');
+const btnSave = document.getElementById('btnSave');
+const post = document.getElementById('post');
+const posts = document.getElementById('posts');
 
 window.onload = () => {
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       console.log('User is signed in.');
       login.classList.add("hiden");
+      bd.classList.remove("hiden");
+      posts.classList.remove("hiden");
       logout.classList.remove("hiden");
       username.innerHTML = `Bienvenida ${user.displayName}`;
     } else {
       console.log('No user is signed in.');
       login.classList.remove("hiden");
       logout.classList.add("hiden");
+      posts.classList.add("hiden");
+      bd.classList.add("hiden")
     }
   });
 }
+
+
+
+
+function writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
+}
+
+
+function writeNewPost(uid, body) {
+  // A post entry.
+  var postData = {
+    uid: uid,
+    body: body,
+  };
+
+  // Get a key for a new Post.
+  var newPostKey = firebase.database().ref().child('posts').push().key;
+
+  // Write the new post's data simultaneously in the posts list and the user's post list.
+  var updates = {};
+  updates['/posts/' + newPostKey] = postData;
+  updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+
+  return firebase.database().ref().update(updates);
+}
+
+btnSave.addEventListener('click', () => {
+  var userId = firebase.auth().currentUser.uid;
+  writeNewPost(userId, post.value );
+
+  var btnUpdate = document.createElement("input");
+  btnUpdate.setAttribute("value", "Update");
+  btnUpdate.setAttribute("type", "button");
+  var btnDelete = document.createElement("input");
+  btnDelete.setAttribute("value", "Delete");
+  btnDelete.setAttribute("type", "button");
+  var contPost = document.createElement('div');
+
+  contPost.innerHTML = `<textarea name="Publicacòn" id="post" cols="30" rows="10">${post.value}</textarea>`;
+
+  contPost.appendChild(btnUpdate );
+  contPost.appendChild(btnDelete);
+  posts.appendChild(contPost);
+})
 
 btnRegister.addEventListener('click', () => {
   firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
@@ -72,17 +128,16 @@ btnFacebook.addEventListener('click', () => {
 btnGoogle.addEventListener('click',() => {
   var provider = new firebase.auth.GoogleAuthProvider();
 
-  //provider.setCustomParameters({
-  //  'login_hint': 'user@example.com'
-  //});
-
   firebase.auth().signInWithPopup(provider)
-    .then(function(result) { console.log('Login Google') })
+    .then(function(result) {
+      console.log('Login Google');
+      var user = result.user;
+      writeUserData(user.uid, user.displayName, user.email, user.photoURL);
+     })
     .catch(function(error) {
       console.log(error.code);
       console.log(error.message);
       console.log(error.email);
       console.log(error.credential);
   });
-
 });
